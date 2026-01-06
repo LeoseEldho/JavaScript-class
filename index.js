50989f8a4caa5284e3e43cd574951cdd7c296bc0
Leose <leoseeldho@gmail.com>
2026-01-06 14:16:34 +0530

    06-01-2026

--- index.js
+++ index.js
@@ -513,5 +513,39 @@
 // sdfewasdfw
 // ---------------------------------------------------------------------------------------------------------------------------
 
+// Q2. First Non-Repeating Window Character
+// Given a string S and integer K,
+//  for every substring of size K, print the first non-repeating character.
+// If none exists, print #.
+// Input
 
-// asdfasdfasdfasdfasdfasdfasdf
+// S = "aabcdbec"
+// K = 4
+// Output
+// a b c d d e
+
+
+function nonRepeatingFixed(s, k) {
+    let freq = {};
+    let start = 0;
+    let result = ""
+    for (let j = 0; j < s.length; j++) {
+        freq[s[j]]=(freq[s[j]]||0)+1
+        if (j - start + 1 === k) {
+            let found = '#';
+            for (let i = start; i <= j; i++) {
+                if (freq[s[i]] === 1) {
+                    found = s[i];
+                    break;
+                }
+            }
+            result += ' '+found;
+            freq[s[start]]--;
+            start++;
+        }
+    }
+    return result;
+}
+console.log(nonRepeatingFixed("aabcdbec", 4));
+
+//    sdfsdfsdfsdfsdfasdfsdfsdfsdfsdfasdfasdfasdfasdfsdfsfasdfxczxvzxcvxcvzxcvxcvxccvxcv
