bd70682bb7453cde3984592105508c7c79885f11
Leose <leoseeldho@gmail.com>
2026-03-06 20:14:15 +0530

    06-03-2026

--- index.js
+++ index.js
@@ -513,30 +513,41 @@
 // sdfewasdfw
 // ---------------------------------------------------------------------------------------------------------------------------
 
+// let arr = [1, 3, 2, 2, 2, 3,4,4,4,4,4,4];
+// function nonRepeat(arr) {
+//   let max = 0
+//   let repe=0
+//   let obj={}
+//   for (let i = 0; i < arr.length; i++){
+//     obj[arr[i]]=(obj[arr[i]]||0)+1
+//   }
+//   for (let key in obj) {
+//     if (obj[key] > max) {
+//       max = obj[key]
+//       repe = key
+//     }
+//   }
+//   console.log(obj)
+//   return repe
+// }
+// console.log(nonRepeat(arr))
 
-function reverseGravity(grid) {
-    let rows = grid.length;
-    let cols = grid[0].length;
-    for (let i = 0; i < cols; i++) {
-        let count = 0;
-        for (let j = 0; j < rows; j++) {
-            if (grid[j][i] == "#") {
-                count++;
-            }
-        }
-        for (let k = 0; k < rows; k++){
-            if (k<count) {
-                grid[k][i]='#'
-            } else {
-                grid[k][i]="."
+let arr = [1,2,3,4,5,4];
+
+function removeDuplicates(arr) {
+    let max = arr[0]
+    let second = Infinity
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] > max) {
+            second = max;
+            max=arr[i]
+        } else {
+            if (arr[i] > second && arr[i] !== max) {
+                second=arr[i]
             }
         }
     }
-    return grid;
+    
+    return second
 }
-let grid = [
-    ['.', '.', '#'],
-    ['.', '#', '.'],
-    ['#', '.', '#']
-];
-console.log(reverseGravity(grid));
+console.log(removeDuplicates(arr))
