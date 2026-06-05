27c23f9091abd96f3cb24c9718c33e5cba665d2d
Leose <leoseeldho@gmail.com>
2026-06-05 09:48:11 +0530

    05-06-2026

---
+++ Java/demo.java
@@ -0,0 +1,11 @@
+public class demo {
+    public static void main(String[] argument) {
+        int x = 10;
+        int y = 20;
+        boolean result = x < y && x == 10;
+        System.out.println("" + result);
+         
+        boolean a = x < y || y == 20;
+        System.out.println("" + a);
+    }
+}
---
+++ Java/operator.java
@@ -0,0 +1,33 @@
+// class operator {
+//     public static void main(String[] argument) {
+        
+//         int x = 4;
+//         x *= 6;
+//         int z = x;
+//         System.out.println(""+z+" "+x);
+
+//     }
+// };
+
+class add {
+    public static void main(String[] argument) {
+        
+        int x = 4;
+        int y = 6;
+        int z = x + y;
+        System.out.println("sum :" + z);
+        
+        System.out.println(x-2);
+        System.out.println(y*5);
+        System.out.println(10 / 5);
+        int a = 4;
+        int b = 4;
+        System.out.println(a == b);
+        System.out.println(a<b);
+        System.out.println(10 > 5);
+        for (int i = 0; i <= 10; i++) {
+        System.out.println(i);
+            
+        }
+    }
+}
