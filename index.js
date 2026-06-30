bc3bba67ceaabebcd04a7b83dc8aa5242e17fcfc
Leose <leoseeldho@gmail.com>
2026-06-30 09:33:33 +0530

    30-06-2026

--- index.js
+++ index.js
@@ -709,14 +709,19 @@
 //     return result
 // };
 // console.log(compressString("aaaabbcc"))
+ 
+const strcom = (str) => {
+    let result = "";
+    let count = 1;
+    for (let i = 0; i < str.length; i++){
+        if (str[i] == str[i + 1]) {
+            count++;
+        } else {
+            result += str[i] + count;
+        count = 1;
 
-// const expandString = (str) => {
-//     let result = "";
-//     let count = 0
-//     for (let i = 0; i < str.length; i+=2){
-//         result+=str[i].repeat(str[i+1])
-//     }
-//     return result
-// };
-// console.log(expandString("a4b2c3"));
-// output="aaaabbcc"
+        }
+    }
+    return result;
+};
+console.log(strcom("aaaabbcc"))
